4b7da42911a941650a3b21b01fdcc39ccfa12d32
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2020-04-18 14:28:14 -0400

    Prepare to implement CBZ

--- mangaDown.py
+++ mangaDown.py
@@ -3,12 +3,12 @@
 
 import requests, os, time, img2pdf
 from selenium import webdriver
-
-baseUrl = "https://kissmanga.com/Manga/"
-os.makedirs("manga", exist_ok=True)  # store comics in ./manga
+import zipfile
 
 
 def downloadManga(mangaName, create_pdf, create_cbz):
+    baseUrl = "https://kissmanga.com/Manga/"
+    os.makedirs("manga", exist_ok=True)
     os.chdir("manga")
     base_filename = mangaName.split("/")[0]
     os.makedirs(base_filename, exist_ok=True)
@@ -16,7 +16,7 @@ def downloadManga(mangaName, create_pdf, create_cbz):
     print("Downloading page %s..." % baseUrl + mangaName, end="")
     browser = webdriver.Firefox()
     browser.get(baseUrl + mangaName)  # handle exceptions here
-    time.sleep(15) # kissmanaga takes time to load (browser check) - give it time 
+    time.sleep(15)  # kissmanaga takes time to load (browser check) - give it time
     print(" - Done")
     # Find the URL of the comic image.
     comicElem = browser.find_element_by_css_selector("#divImage")
@@ -40,7 +40,7 @@ def downloadManga(mangaName, create_pdf, create_cbz):
             imageFile = open(
                 os.path.join(
                     base_filename,
-                    os.path.basename(base_filename + "_" + str(k) + ".jpg"),
+                    os.path.basename(base_filename + "-" + str(k) + ".jpg"),
                 ),
                 "wb",
             )
@@ -52,15 +52,19 @@ def downloadManga(mangaName, create_pdf, create_cbz):
     # creating a pdf
     os.chdir(base_filename)
     dl_file_list = [i for i in os.listdir(".") if i.endswith(".jpg")]
-    with open(base_filename + ".pdf", "wb") as f:
-        f.write(img2pdf.convert(dl_file_list))
-    browser.quit() # The program is done, close the web browser.
+    if create_pdf:
+        with open(base_filename + ".pdf", "wb") as f:
+            f.write(img2pdf.convert(dl_file_list))
+    if create_cbz:
+        pass
+    browser.quit()  # The program is done, close the web browser.
     print("Done")
+
 
 # eg - Hajime-no-Ippo/Ch-1239----In-His-Hand
 name = input("Enter manga name and chapter no. in kissmanga format: ")
-mk_pdf = input("Create PDF? [Y/n]").strip().lower().startswith("y")
-mk_cbz = input("Create CBZ? [Y/n]").strip().lower().startswith("y")
+mk_pdf = input("Create PDF? [y/n] ").strip().lower().startswith("y")
+mk_cbz = input("Create CBZ? [y/n] ").strip().lower().startswith("y")
 if not mk_cbz and not mk_pdf:
     print("You must create some type of file, defaulting to PDF")
     mk_pdf = True
